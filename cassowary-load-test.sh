9eb8b96a0aa30b9b3242a1445bf90eb577e6d439
PandaDinabandhu <31442648+PandaDinabandhu@users.noreply.github.com>
2024-11-29 16:40:35 +0530

    Update cassowary-load-test.sh

--- cassowary-load-test.sh
+++ cassowary-load-test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/opt/homebrew/bin/cassowary run -u http://35.192.219.205/ -c 1000 -n 1000 --duration 100
+/opt/homebrew/bin/cassowary run -u http://35.225.145.219/ -c 1000 -n 1000 --duration 100
